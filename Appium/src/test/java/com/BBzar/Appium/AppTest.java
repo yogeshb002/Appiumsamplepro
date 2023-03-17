c581bba0b21beee3557a48569bad43cfa28586f1
Yogesh <Yogesh@DESKTOP-0DRJ7O2>
2023-03-17 14:57:33 +0530

    First Commit

--- Appium/src/test/java/com/BBzar/Appium/AppTest.java
+++ Appium/src/test/java/com/BBzar/Appium/AppTest.java
@@ -1,20 +1,12 @@
 package com.BBzar.Appium;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+   
+	@Test
+    public void Abcd(){
+    
+        System.out.println("First Demo Project");
     }
 }
